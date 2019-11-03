52a079134907e1943d933e97592a249bd3052c8b
eien <GSKryachko@gmail.com>
2019-11-03 20:40:12 +0500

    Added groups table to schema.sql, removed unique constraints from file and user names

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -15,25 +15,27 @@ create table files
     id serial not null,
     name varchar(100) not null,
     creator int not null,
-    description varchar(100)
+    description varchar(100),
+    path varchar(256) not null
 );
 
 create unique index files_id_uindex
     on files (id);
 
-create unique index files_name_uindex
-    on files (name);
-
-create unique index files_name_uindex_2
-    on files (name);
-
-alter table files
-    add constraint files_pk
-        unique (name);
-
 alter table files
     add constraint files_users_id_fk
         foreign key (creator) references users;
+
+create table groups
+(
+    id   serial  not null
+        constraint groups_pk
+            primary key,
+    name varchar(100) not null
+);
+
+create unique index groups_id_uindex
+    on groups (id);
 
 create table user_files
 (
@@ -45,4 +47,28 @@ create table user_files
             references files (id),
     constraint user_files_pk
         unique (user_id, file_id)
+);
+
+create table user_groups
+(
+    user_id int not null
+        constraint user_groups_users_id_fk
+            references users,
+    file_id int not null
+        constraint user_groups_groups_id_fk
+            references groups (id),
+    constraint user_groups_pk
+        unique (user_id, group_id)
+);
+
+create table group_files
+(
+    group_id int not null
+        constraint group_files_groups_id_fk
+            references groups,
+    file_id int not null
+        constraint group_files_files_id_fk
+            references files (id),
+    constraint group_files_pk
+        unique (group_id, file_id)
 );
